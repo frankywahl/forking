fb9c8d8ab245f085a7786403b934221758fec62a
Franky W. (franklinwahl - Frankys-MacBook-Air.local) <frankywahl@users.noreply.github.com>
2015-03-07 09:21:30 -0800

    Adding URI parser

--- lib/forking/repository.rb
+++ lib/forking/repository.rb
@@ -1,13 +1,29 @@
 module Forking
   class Repository
+    class NotImplementedError < StandardError; end
+
+    def directory
+      fail NotImplementedError, "This #{self.class} cannot respond to:"
+    end
+
+    def user
+      fail NotImplementedError, "This #{self.class} cannot respond to:"
+    end
+
+    def domain
+      fail NotImplementedError, "This #{self.class} cannot respond to:"
+    end
+
+    def ssh_user
+      nil
+    end
+  end
+
+  class SSHRepository < Repository
     attr_accessor :uri
 
     def initialize(uri)
       @uri = uri
-    end
-
-    def repo
-      options[:repo]
     end
 
     def directory
@@ -24,6 +40,27 @@ module Forking
 
     def ssh_user
       @ssh_user ||= uri.split('@').first
+    end
+  end
+
+  class URIRepository < Repository
+    attr_accessor :uri
+
+    def initialize(uri)
+      require 'uri'
+      @uri = URI.parse(uri)
+    end
+
+    def domain
+      uri.host
+    end
+
+    def user
+      uri.path.split('/')[1]
+    end
+
+    def directory
+      uri.path.split('/')[2..-1].join('').gsub(/\.git$/, '')
     end
   end
 end
--- spec/repository_spec.rb
+++ spec/repository_spec.rb
@@ -1,21 +1,75 @@
 require_relative 'spec_helper'
 
-describe Forking::Repository do
-  let(:repo) { described_class.new('git@github.com:frankywahl/forking.git') }
+describe Forking do
+  describe described_class::Repository do
+    let(:repo) { described_class.new }
 
-  it '#user' do
-    expect(repo.user).to eql 'frankywahl'
+    [:domain, :directory, :user, :ssh_user].each do |method|
+      it "responds to #{method}" do
+        expect(repo).to respond_to method
+      end
+    end
+
+    [:domain, :directory, :user].each do |method|
+      it "#{method} raise an error"  do
+        expect { repo.send(method) }.to raise_error(Forking::Repository::NotImplementedError)
+      end
+    end
   end
 
-  it '#directory' do
-    expect(repo.directory).to eql 'forking'
+  describe described_class::SSHRepository do
+    let(:repo) { described_class.new('git@github.com:frankywahl/forking.git') }
+
+    it 'is a repository' do
+      expect(repo).to be_a Forking::Repository
+    end
+
+    it '#user' do
+      expect(repo.user).to eql 'frankywahl'
+    end
+
+    it '#directory' do
+      expect(repo.directory).to eql 'forking'
+    end
+
+    it '#domain' do
+      expect(repo.domain).to eql 'github.com'
+    end
+
+    it '#ssh_user' do
+      expect(repo.ssh_user).to eql 'git'
+    end
+
+    context 'Github enterprise' do
+      let(:repo) { described_class.new('git@my.domain.co.uk:frankywahl/forking.git') }
+
+      it '#domain' do
+        expect(repo.domain).to eql 'my.domain.co.uk'
+      end
+    end
   end
 
-  it '#domain' do
-    expect(repo.domain).to eql 'github.com'
-  end
+  describe described_class::URIRepository do
+    let(:repo) { described_class.new('https://github.com/frankywahl/forking.git') }
 
-  it '#ssh_user' do
-    expect(repo.ssh_user).to eql 'git'
+    it 'is a repository' do
+      expect(repo).to be_a Forking::Repository
+    end
+
+    it '#user' do
+      expect(repo.user).to eql 'frankywahl'
+    end
+
+    it '#domain' do
+      expect(repo.domain).to eql 'github.com'
+    end
+
+    it '#directory' do
+      expect(repo.directory).to eql 'forking'
+    end
+
+    it '#ssh_user' do
+      expect(repo.ssh_user).to be_nil
+    end
   end
 end
